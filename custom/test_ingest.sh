200c00a249a19de900450f4086187d1c1afef8ce
yangyang <yangyangjishu@blued.com>
2022-06-30 16:00:32 +0800

    add test_ingest.sh

---
+++ custom/test_ingest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# usage: ./ingest.sh
+
+source .env
+
+docker run --rm \
+	-v ${PWD}/venv:/venv \
+	-v ${PWD}/pipeline.yml:pipeline.yml \
+	acryldata/datahub-actions:${ACTIONS_VERSION} \
+	/venv/bin/python -m datahub --debug ingest -c pipeline.yml
